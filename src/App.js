c1c19ad9bc98fc58c9330bd837ae3d7071ac2446
Vido <vidoseaver@gmail.com>
2018-07-29 13:27:09 -0600

    Add logout functionality with spinner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Firebase  from "firebase";
 
-import { Header } from "./components/common";
+import { Header, Button, Spinner, CardSection } from "./components/common";
 import LoginForm from "./components/LoginForm";
 
 export default class App extends Component{
+    state = {
+        loggedIn: null
+    }
+
     UNSAFE_componentWillMount(){
         Firebase.initializeApp(
             {
@@ -17,14 +21,54 @@ export default class App extends Component{
                 messagingSenderId: "488639024817"
             }
         )
+
+        Firebase.auth().onAuthStateChanged((user) =>{
+            if (user) {
+                this.setState({
+                    loggedIn: true
+                })
+            } else {
+                this.setState({
+                    loggedIn: false
+                })
+            }
+        })
+    }
+
+    renderContent(){   
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => Firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                )
+            case false:
+                return <LoginForm />
+            default:
+                return(
+                  <View styles={styles.spinnerStyle} >
+                        <Spinner size='large' />
+                  </View>  
+                ) 
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    spinnerStyle: {
+        alignItems:'center',
+        alignSelf: 'center',
+        justifyContent: 'center'
+    }
+})
